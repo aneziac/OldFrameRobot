b16aa8de6cf7df60a7688037fc45fa621edbc460
SaidGLopez12 <99686473+SaidGLopez12@users.noreply.github.com>
2022-02-28 08:23:11 -0800

    Controls
    My first attempt at getting the controlls to work for the robot. Moves foward and backwards for the front left and right motors.

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,11 @@
 /*----------------------------------------------------------------------------*/
 
 // ---- START VEXCODE CONFIGURED DEVICES ----
+// Robot Configuration:
+// [Name]               [Type]        [Port(s)]
+// Controller1          controller                    
+// leftFrontMotor       motor         1               
+// rightFrontMotor      motor         2               
 // ---- END VEXCODE CONFIGURED DEVICES ----
 
 #include "vex.h"
@@ -16,6 +21,40 @@ using namespace vex;
 
 int main() {
   // Initializing Robot Configuration. DO NOT REMOVE!
+  while (1 == 1)
+  {
+    leftFrontMotor.spin(directionType::fwd, Controller1.Axis3.value(),velocityUnits::pct);
+    rightFrontMotor.spin(directionType::fwd, Controller1.Axis2.value(),velocityUnits::pct);
+
+    // Left Front Motor Code. Moves foward and backwards.
+    if(Controller1.ButtonL1.pressing())
+    {
+      leftFrontMotor.spin(directionType::fwd, 50 , velocityUnits::pct);
+    }
+    else if(Controller1.ButtonL2.pressing())
+    {
+      leftFrontMotor.spin(directionType::rev, 50 , velocityUnits::pct);
+    }
+    else
+    {
+      leftFrontMotor.stop(brakeType::hold);
+    }
+
+     // Left Front Motor Code. Moves foward and backwards.
+    if(Controller1.ButtonR1.pressing())
+    {
+      rightFrontMotor.spin(directionType::fwd, 50 , velocityUnits::pct);
+    }
+    else if(Controller1.ButtonR2.pressing())
+    {
+      rightFrontMotor.spin(directionType::rev, 50 , velocityUnits::pct);
+    }
+    else
+    {
+      rightFrontMotor.stop(brakeType::hold);
+    }
+
+  }
   vexcodeInit();
   // hello.
   
