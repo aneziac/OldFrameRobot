fc6a4fc83ab46e7ea6ac68fad08918576cc3129c
aneziac <nateannau@gmail.com>
2022-03-03 16:02:58 -0800

    Readd my commits

--- include/robot-config.h
+++ include/robot-config.h
@@ -13,7 +13,7 @@ extern motor29 clawMotor;
 
 /**
  * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
- * 
+ *
  * This should be called at the start of your int main function.
  */
-void  vexcodeInit( void );
+void vexcodeInit(void);
