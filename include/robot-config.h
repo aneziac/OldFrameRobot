95fe726ff89b6fac6153fd788a415291ee5d4cb3
SaidGLopez12 <99686473+SaidGLopez12@users.noreply.github.com>
2022-03-14 13:40:23 -0700

    Current Changes to the controls.
    Implemented tank controls and assigned arm and chain motors to trigger buttons. Have not tried it out.

--- include/robot-config.h
+++ include/robot-config.h
@@ -5,21 +5,19 @@ extern brain Brain;
 using signature = vision::signature;
 
 // VEXcode devices
-extern motor leftFrontMotor;
-extern motor leftBackMotor;
-extern motor rightFrontMotor;
-extern motor rightBackMotor;
-extern motor29 armMotor;
-extern motor29 clawMotor;
 extern controller Controller1;
-extern signature Vision1__COLORRED;
-extern signature Vision1__SIG_2;
-extern signature Vision1__COLORBLUE;
-extern signature Vision1__SIG_4;
-extern signature Vision1__SIG_5;
-extern signature Vision1__SIG_6;
-extern signature Vision1__SIG_7;
-extern vision Vision1;
+extern signature robotEyes__COLORRED;
+extern signature robotEyes__SIG_2;
+extern signature robotEyes__COLORBLUE;
+extern signature robotEyes__SIG_4;
+extern signature robotEyes__SIG_5;
+extern signature robotEyes__SIG_6;
+extern signature robotEyes__SIG_7;
+extern vision robotEyes;
+extern motor armMotor;
+extern motor chainMotor;
+extern motor leftWheels;
+extern motor rightWheels;
 
 /**
  * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
--- src/robot-config.cpp
+++ src/robot-config.cpp
@@ -8,23 +8,61 @@ using code = vision::code;
 brain  Brain;
 
 // VEXcode device constructors
-motor leftFrontMotor = motor(PORT11, ratio18_1, false);
-motor leftBackMotor = motor(PORT12, ratio18_1, false);
-motor rightFrontMotor = motor(PORT1, ratio18_1, false);
-motor rightBackMotor = motor(PORT2, ratio18_1, false);
-motor29 armMotor = motor29(Brain.ThreeWirePort.H, false);
-motor29 clawMotor = motor29(Brain.ThreeWirePort.A, false);
 controller Controller1 = controller(primary);
 /*vex-vision-config:begin*/
-signature Vision1__COLORRED = signature (1, 7447, 9661, 8554, -1721, -629, -1176, 2.9, 0);
-signature Vision1__SIG_2 = signature (2, 0, 0, 0, 0, 0, 0, 3, 0);
-signature Vision1__COLORBLUE = signature (3, -3201, -2625, -2914, 5139, 6767, 5952, 2, 0);
-vision Vision1 = vision (PORT15, 50, Vision1__COLORRED, Vision1__SIG_2, Vision1__COLORBLUE);
+signature robotEyes__COLORRED = signature (1, 7447, 9661, 8554, -1721, -629, -1176, 2.9, 0);
+signature robotEyes__SIG_2 = signature (2, 0, 0, 0, 0, 0, 0, 3, 0);
+signature robotEyes__COLORBLUE = signature (3, -3201, -2625, -2914, 5139, 6767, 5952, 2, 0);
+vision robotEyes = vision (PORT15, 50, robotEyes__COLORRED, robotEyes__SIG_2, robotEyes__COLORBLUE);
 /*vex-vision-config:end*/
+motor armMotor = motor(PORT16, ratio18_1, false);
+motor chainMotor = motor(PORT17, ratio18_1, false);
+motor leftWheels = motor(PORT1, ratio18_1, false);
+motor rightWheels = motor(PORT11, ratio18_1, false);
 
 // VEXcode generated functions
 // define variable for remote controller enable/disable
 bool RemoteControlCodeEnabled = true;
+// define variables used for controlling motors based on controller inputs
+bool Controller1LeftShoulderControlMotorsStopped = true;
+bool Controller1RightShoulderControlMotorsStopped = true;
+
+// define a task that will handle monitoring inputs from Controller1
+int rc_auto_loop_function_Controller1() {
+  // process the controller input every 20 milliseconds
+  // update the motors based on the input values
+  while(true) {
+    if(RemoteControlCodeEnabled) {
+      // check the ButtonL1/ButtonL2 status to control armMotor
+      if (Controller1.ButtonL1.pressing()) {
+        armMotor.spin(forward);
+        Controller1LeftShoulderControlMotorsStopped = false;
+      } else if (Controller1.ButtonL2.pressing()) {
+        armMotor.spin(reverse);
+        Controller1LeftShoulderControlMotorsStopped = false;
+      } else if (!Controller1LeftShoulderControlMotorsStopped) {
+        armMotor.stop();
+        // set the toggle so that we don't constantly tell the motor to stop when the buttons are released
+        Controller1LeftShoulderControlMotorsStopped = true;
+      }
+      // check the ButtonR1/ButtonR2 status to control chainMotor
+      if (Controller1.ButtonR1.pressing()) {
+        chainMotor.spin(forward);
+        Controller1RightShoulderControlMotorsStopped = false;
+      } else if (Controller1.ButtonR2.pressing()) {
+        chainMotor.spin(reverse);
+        Controller1RightShoulderControlMotorsStopped = false;
+      } else if (!Controller1RightShoulderControlMotorsStopped) {
+        chainMotor.stop();
+        // set the toggle so that we don't constantly tell the motor to stop when the buttons are released
+        Controller1RightShoulderControlMotorsStopped = true;
+      }
+    }
+    // wait before repeating the process
+    wait(20, msec);
+  }
+  return 0;
+}
 
 /**
  * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
@@ -32,5 +70,5 @@ bool RemoteControlCodeEnabled = true;
  * This should be called at the start of your int main function.
  */
 void vexcodeInit( void ) {
-  // nothing to initialize
+  task rc_auto_loop_task_Controller1(rc_auto_loop_function_Controller1);
 }
