f7a204a5dd64061a0aae18a4f03542e7d9bc18df
aneziac <nateannau@gmail.com>
2022-04-07 17:16:54 -0700

    Basic turning

--- src/main.cpp
+++ src/main.cpp
@@ -102,12 +102,29 @@ void identifyObjects(void) {
   Brain.Screen.render();
 }
 
+// turn the robot left a little bit some number of times
+void turnLeft(const int times) {
+  for (int i = 0; i < times; i++) {
+    leftWheels.spin(directionType::rev);
+    rightWheels.spin(directionType::fwd);
+  }
+}
+
+// turn the robot right a little bit some number of times
+void turnRight(const int times) {
+  for (int i = 0; i < times; i++) {
+    leftWheels.spin(directionType::fwd);
+    rightWheels.spin(directionType::rev);
+  }
+}
+
+
 int main() {
   // Initializing Robot Configuration. DO NOT REMOVE!
   vexcodeInit();
 
   // Draws gear logo
-  drawLogo();
+  // drawLogo();
   /*
   // creates a group of motors for each side (not needed since we currently have 1 motor each side)
   motor_group leftSideMotors = motor_group(leftFrontMotor, leftBackMotor);
@@ -119,7 +136,7 @@ int main() {
   while(running)
   {
     // Identify objects
-    // identifyObjects();
+    identifyObjects();
 
     // left analog stick uses axis 3 (up and down) and axis 4 (left and right)
     // right analog stick uses axis 2 (up and down) and axis 1 (left and right)
